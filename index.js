2bc57e396fe0c1f636858224dfb394c926f4b492
Hunter Kassow <hunterkassow@protonmail.com>
2022-05-17 16:01:20 -0400

    commit tree 18b9d8db17ad0206e03f89db013b16da85ab3c7a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    let winningElement = record.find(element => element.result === 'W');
+    if (!!winningElement) {
+        return winningElement.year;
+    }
+    else {
+        return undefined;
+    }
+    
+}
